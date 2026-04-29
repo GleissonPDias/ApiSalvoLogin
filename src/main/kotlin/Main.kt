7e194ff5e321312e0d77cb4e813cfb20b9140810
GleissonPDias <gleisson.gpd10@gmail.com>
2026-04-29 20:18:48 -0300

    Adição do get para pedidos.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 package com.example
 
+
 import com.example.routes.authRoutes
+import com.example.routes.pedidoRoutes
 import com.example.routes.matchRoutes
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
@@ -8,7 +10,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
-import pedidoRoutes
+
+
 
 fun main() {
     val port = System.getenv("PORT")?.toInt() ?: 8080
@@ -22,8 +25,9 @@ fun main() {
         routing {
             // Chamando as rotas que foram divididas nas outras pastas
             authRoutes()
-            matchRoutes()
             pedidoRoutes()
+            matchRoutes()
+
         }
 
     }.start(wait = true)
